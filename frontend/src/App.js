c04bb56f6acbfb4ca305b28d0123305bf68b1ad3
Irina <irinakhaus@gmail.com>
2023-10-11 00:08:17 +0300

    Add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Привет, мир! 👋</h1>
+      <p>Это мое первое приложение на React.</p>
+    </div>
+  );
+}
+
+export default App;
