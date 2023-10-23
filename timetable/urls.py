96d1e500ed7ceaff3b8e9aab0f330891ff6cb885
Irina <irinakhaus@gmail.com>
2023-10-23 20:31:05 +0300

    Update urls

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from django.conf import settings
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='timetable/index.html'), name='home'),
 ]
-
-if settings.DEBUG: 
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
