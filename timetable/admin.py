7573eeae76fb0c9594776a74104585ee716363a8
Irina <irinakhaus@gmail.com>
2023-10-23 19:59:37 +0300

    Update admin

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from . models import Timetable, Classroom, Subject, Group, WorkType, Tutor, JsonTimetable
+from django.db import transaction
+from .utils import upload_data
+from .models import Timetable, Classroom, Subject, Group, WorkType, Tutor, JsonTimetable
 
 admin.site.register(Classroom)
 admin.site.register(Subject)
@@ -7,7 +9,15 @@ admin.site.register(Group)
 admin.site.register(Tutor)
 admin.site.register(Timetable)
 admin.site.register(WorkType)
-admin.site.register(JsonTimetable)
 
 admin.site.site_title = "Администрирование расписания в университете"
 admin.site.site_header = "Администрирование расписания в университете"
+
+@admin.register(JsonTimetable)
+class JsonTimeTableAdmin(admin.ModelAdmin):
+    list_display = ('json_file', 'date_time_of_upload',)
+    
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if not change:
+            transaction.on_commit(lambda: upload_data(obj))
---
+++ timetable/utils.py
@@ -0,0 +1,137 @@
+from datetime import datetime, timedelta
+from json import loads
+
+from .models import Group, Timetable, Tutor, Subject, Classroom, WorkType, JsonTimetable
+
+
+def daterange(start_date, end_date):
+    """
+    Принимает начальную и конечную дату, генерирует даты из данного промежутка.
+    Используется для формирования словаря с расписанием по дням.
+    """
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+        
+        
+def filter_by_day(from_date, to_date, timetable):
+    """
+    Принимает две даты, обозначающие начало заданного периода, и расписание, которое фильтрует в соответствии с ними.
+    Возвращает словарь, где ключи - даты, полученные из функции daterange(), значения - отфильтрованное по ним расписание.
+    """
+    if from_date:
+        timetable = timetable.filter(work_day__gte=from_date)
+    if to_date:
+        timetable = timetable.filter(work_day__lte=to_date) 
+               
+    timetable_days = {}
+    start = datetime.strptime(from_date, "%Y-%m-%d").date() if from_date else datetime.now().date()
+    end = datetime.strptime(to_date, "%Y-%m-%d").date() if to_date else datetime.now().date()
+    for single_date in daterange(start, end + timedelta(days=1)):
+        timetable_days[single_date] = timetable.filter(work_day=single_date)
+    return timetable_days
+
+
+
+def json_to_dict(file): 
+    """
+    Принимает json-файл, который преобразует в объект Python и на его основе формирует словарь с расписанием по датам.
+    """
+    scheduler = loads(file.json_file.read())
+    
+    timetable = {}
+    all_tutors = set()
+    all_subjects = set()
+    all_groups = set()
+    all_classrooms = set()
+    all_work_types = set()
+    
+    for one_day in scheduler["sheduler"]:
+        year = str(one_day["workYear"])
+        month = str(one_day["workDate"])
+        day = str(one_day["workMonth"])
+        
+        date = datetime.strptime(year + month + day, "%Y%m%d").date()
+        timetable[date] = []
+        
+        for routine in one_day["workSheduler"]:
+            daily_routine = {}
+            daily_routine["work_start"] = datetime.strptime(routine["workStart"], "%H:%M").time()
+            daily_routine["work_end"] = datetime.strptime(routine["workEnd"], "%H:%M").time()
+            daily_routine["work_type"] = routine["workType"]
+            daily_routine["subject"] = routine["area"]
+            daily_routine["tutor"] = routine["tutor"]
+            daily_routine["classroom"] = routine["place"]
+            daily_routine["hours"] = routine["hours"]
+            
+            all_subjects.add(daily_routine["subject"])
+            all_tutors.add(daily_routine["tutor"])
+            all_classrooms.add(daily_routine["classroom"])
+            all_work_types.add(daily_routine["work_type"])
+            
+            groups_temp = []
+            for groups in routine["groups"]:
+                groups_temp.append(groups["groupNum"])  
+                all_groups.add(groups["groupNum"])   
+            if len(groups_temp) == 1:
+                daily_routine["group"] = "".join(groups_temp)
+                timetable[date].append(daily_routine)
+            else:
+                for group in groups_temp:         
+                    daily_routine["group"] = group
+                    timetable[date].append(daily_routine.copy())
+                    
+    return timetable, all_tutors, all_subjects, all_groups, all_classrooms, all_work_types
+
+
+def upload_data(file):
+    
+    """ 
+    Загружает json-файл с расписанием на сервер и обрабатывает его с помощью функции json_to_dict(), получая само расписание в виде
+    словаря, где ключ - дата, значение - расписание на неё, а также множества, содержащие все объекты расписания.
+    Для каждого из расписания и объектов расписания проверяет его существование во всех объектах модели, и если его не существует, 
+    то создаёт новый объект.
+    """
+    timetable, all_tutors, all_subjects, all_groups, all_classrooms, all_work_types = json_to_dict(file)
+
+    for tutor in all_tutors:
+        if not Tutor.objects.filter(tutor_name = tutor).exists():  
+            Tutor.objects.create(tutor_name = tutor)
+    
+    for group in all_groups:
+        if not Group.objects.filter(group_num = group).exists(): 
+            Group.objects.create(group_num = group)
+    
+    for subject in all_subjects:
+        if not Subject.objects.filter(subject_name = subject).exists(): 
+            Subject.objects.create(subject_name = subject)
+            
+    for classroom in all_classrooms:
+        if not Classroom.objects.filter(place = classroom).exists(): 
+            Classroom.objects.create(place = classroom)
+            
+    for work_type in all_work_types:
+        if not WorkType.objects.filter(work_type = work_type).exists(): 
+            WorkType.objects.create(work_type = work_type)
+
+    for date, timetables_for_date in timetable.items():
+        for each_timetable in timetables_for_date:
+            if not Timetable.objects.filter(
+                work_start = each_timetable["work_start"],
+                work_end = each_timetable["work_end"],
+                work_day = date,
+                tutor = Tutor.objects.get(tutor_name = each_timetable["tutor"]),
+                group = Group.objects.get(group_num = each_timetable["group"]),
+                classroom = Classroom.objects.get(place = each_timetable["classroom"]),
+                subject = Subject.objects.get(subject_name = each_timetable["subject"]),
+                work_type = WorkType.objects.get(work_type = each_timetable["work_type"]),
+            ):
+                Timetable.objects.create(
+                work_start = each_timetable["work_start"],
+                work_end = each_timetable["work_end"],
+                work_day = date,
+                tutor = Tutor.objects.get(tutor_name = each_timetable["tutor"]),
+                group = Group.objects.get(group_num = each_timetable["group"]),
+                classroom = Classroom.objects.get(place = each_timetable["classroom"]),
+                subject = Subject.objects.get(subject_name = each_timetable["subject"]),
+                work_type = WorkType.objects.get(work_type = each_timetable["work_type"]),
+            )
