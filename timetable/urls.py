1ac3f8cbcbc6af323b560b628200e60ec97f440b
Irina <irinakhaus@gmail.com>
2023-10-24 15:33:09 +0300

    Update urls

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+app_name = 'timetable'
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='timetable/index.html'), name='home'),
 ]
